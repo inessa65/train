06829342d0b83141f35deb93db2be6afa3b42dd6
Gameinatr <gameinatr@gmail.com>
2020-07-11 11:49:25 +0300

    emailrecovery

---
+++ src/views/EmailRecovery.vue
@@ -0,0 +1,33 @@
+<template>
+  <div className="recovery">
+    <form v-if="!sent" v-on:submit.prevent="submitForm">
+      <div className="form-row">
+        <div className="form-field">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" v-model.trim="email"/>
+        </div>
+      </div>
+      <button type="submit" v-on:click="sendEmail">Отправить e-mail</button>
+    </form>
+    <div v-if="sent" class='success'>
+      <p>{{msg}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'EmailRecovery',
+    data () {
+      return {
+       sent: false,
+       msg: 'E-mail has been succesfully sent!'
+      }
+    },
+    methods:{
+      sendEmail:function(){
+        this.sent = true;
+}
+}
+  }
+</script>
--- src/views/Recovery.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="password">
-        <h1>restore password</h1>
-    </div>
-</template>
-
-
